5d1d75a3c99e24363b6c39f36b9ac759ffb59177
n0160926 <sean.mckeon@libertymutual.com>
2022-01-26 17:15:12 -0500

    CFLL-1220 - fixing paging with empty where

--- src/models/dynamo-page.ts
+++ src/models/dynamo-page.ts
@@ -4,9 +4,11 @@ export class DynamoPage<T> {
     content: T[];
     size: number;
     lastEvaluatedKey?: string;
+    numberOfElements: number;
     constructor (content: T[], pageable: Pageable, lastEvaluatedKey?: string) {
         this.content = content
         this.size = pageable.pageSize
         this.lastEvaluatedKey = lastEvaluatedKey
+        this.numberOfElements = this.content.length
     }
 }
