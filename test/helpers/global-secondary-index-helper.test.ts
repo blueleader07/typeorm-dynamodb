597a858732d7b39ca4c6410d5de2c599dc0cca5f
n0160926 <sean.mckeon@libertymutual.com>
2022-02-23 15:05:14 -0500

    CFLL-1220 - adding support for global secondary indexes

--- test/helpers/global-secondary-index-helper.test.ts
+++ test/helpers/global-secondary-index-helper.test.ts
@@ -1,6 +1,9 @@
 import expect from 'expect'
 import { Dummy } from '../entities/dummy'
-import { indexedColumns } from '../../src/helpers/global-secondary-index-helper'
+import {
+    buildGlobalSecondaryIndexes,
+    indexedColumns
+} from '../../src/helpers/global-secondary-index-helper'
 
 describe('global-secondary-index-helper', () => {
     it('indexedColumns compound partitionKey', async (): Promise<any> => {
@@ -72,5 +75,53 @@ describe('global-secondary-index-helper', () => {
         const sortKeyColumnValue = dummy['lineItemNumber#created']
         expect(partitionKeyColumnValue).toBe('456#staged')
         expect(sortKeyColumnValue).toBe('7#2022-01-01')
+    })
+
+    it('buildGlobalSecondaryIndexSchema compound sortKey', async (): Promise<any> => {
+        /** given: **/
+        const dummy: any = new Dummy()
+        dummy.id = '123'
+        dummy.adjustmentGroupId = '456'
+        dummy.adjustmentStatus = 'staged'
+        dummy.lineItemNumber = 7
+        dummy.created = '2022-01-01'
+
+        const metadata: any = {
+            indices: [
+                {
+                    name: 'adjustmentGroupIdIndex',
+                    columns: [
+                        {
+                            propertyName: 'adjustmentGroupId'
+                        },
+                        {
+                            propertyName: 'adjustmentStatus'
+                        }
+                    ],
+                    where: 'lineItemNumber#created'
+                }
+            ]
+        }
+
+        /** when: **/
+        const schema = buildGlobalSecondaryIndexes(metadata)
+
+        /** then: **/
+        expect(schema).toEqual([{
+            IndexName: 'adjustmentGroupIdIndex',
+            KeySchema: [
+                {
+                    AttributeName: 'adjustmentGroupId#adjustmentStatus',
+                    KeyType: 'HASH'
+                },
+                {
+                    AttributeName: 'lineItemNumber#created',
+                    KeyType: 'RANGE'
+                }
+            ],
+            Projection: {
+                ProjectionType: 'ALL'
+            }
+        }])
     })
 })
