b79ddfbabcf53661c47ec269ef992d1fa32bad9f
Robert Levasseur <Robert.Levasseur@LibertyMutual.com>
2022-06-09 13:09:28 -0400

    removing dead code which is causing build to fail

--- src/parsers/annotation-parser.ts
+++ src/parsers/annotation-parser.ts
@@ -1,26 +1,8 @@
-const removeQuotes = (text: string) => {
-    if (text) {
-        text = text.replace(/"/g, '')
-    }
-    return text
-}
-
 const removeSpaces = (text: string) => {
     let inside = 0
     const s = text.replace(/\n/g, '')
     // eslint-disable-next-line no-return-assign
     return s.replace(/ +|"/g, (m: string) => m === '"' ? (inside ^= 1, '"') : inside ? m : '')
-}
-
-const parseKeyValues = (attribute: string, jsonColumn: any) => {
-    const keyValues = attribute.split(/\s*=\s*/)
-    for (let i = 0; i < keyValues.length; i++) {
-        if (i % 2 === 0) {
-            const key = removeQuotes(keyValues[i])
-            jsonColumn[key] = removeQuotes(keyValues[i + 1])
-        }
-    }
-    return jsonColumn
 }
 
 export const parseAnnotations = (file: string, ...names: string[]) => {
@@ -34,11 +16,9 @@ export const parseAnnotations = (file: string, ...names: string[]) => {
         if (annotation === '@Id') {
             return `name=@Id,index=${index}`
         } else {
-            const annotationAttributes = x[1]
-            return annotationAttributes
+            return x[1]
         }
     })
-    const isId = false
     annotations = annotations.map(annotation => {
         // eslint-disable-next-line no-eval
         return eval('(' + annotation + ')')
